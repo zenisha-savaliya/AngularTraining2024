6aaf14624911db76c0891924ef373c76dd3ece61
zenisha-savaliya <zenisha.savaliya@bacancy.com>
2024-03-17 23:26:35 +0530

    to do list completed

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,8 +1,30 @@
-let inputBox = document.getElementsByClassName("inputBox")[0];
+let inputBox = document.querySelector(".inputBox");
+let taskList = document.querySelector(".taskList");
+
 function addTask() {
-  let list = document.getElementsByClassName("taskList")[0];
-  let listItem = document.createElement("ul");
-  listItem.textContent = inputBox.value;
-  list.appendChild(listItem);
-  inputBox.value = "";
+  let taskText = inputBox.value.trim();
+
+  if (taskText !== "") {
+    let listItem = document.createElement("li");
+    listItem.textContent = taskText;
+    listItem.style.listStyleType = "circle";
+
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "X";
+    removeButton.style.fontWeight = "bold";
+    removeButton.style.fontSize = "16px";
+    removeButton.style.cursor = "pointer";
+    removeButton.style.backgroundColor = "orange";
+    removeButton.style.padding = "5px 10px";
+    removeButton.style.border = "2px solid black";
+    removeButton.style.borderRadius = "50%";
+    removeButton.onclick = function () {
+      listItem.remove();
+    };
+    listItem.appendChild(removeButton);
+
+    taskList.querySelector("ul").appendChild(listItem);
+
+    inputBox.value = "";
+  }
 }
